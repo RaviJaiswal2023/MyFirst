c3b390f50119ffabb710f45073e7ed6f4c009979
RaviJaiswal2023 <raviprakashjaiswal2023@gmail.com>
2025-11-07 09:46:10 +0530

    Add files via upload

---
+++ Pract1a.py
@@ -0,0 +1,12 @@
+# Define profile details
+name = "Ravi Jaiswal"
+age = 34
+profession = "Professor / Software Developer"
+location = "Mumbai, India"
+
+# Print the profile
+print("----- My Profile -----")
+print("Name      :", name)
+print("Age       :", age)
+print(f"Profession: {profession}")
+print("Location  :", location)
